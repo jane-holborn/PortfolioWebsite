cc4468eb87bb582dee3226d37beefe558cf55b72
Jane Holborn <janeholborn@hotmail.com>
2024-11-10 18:01:06 +0800

    Added onclick hamburger style navigation menu.

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    document.querySelector('.hamburgerIcon').classList.toggle('active');  // Toggling on hamburger icon
+    document.querySelector('nav').classList.toggle('active');  // Toggling on nav menu
+}
