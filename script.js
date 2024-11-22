c90801d55bbf8a172757db83bfbeeb71ea9c26c5
Jane Holborn <janeholborn@hotmail.com>
2024-11-22 12:55:08 +0800

    Refactored code.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
     document.querySelector('.hamburgerIcon').classList.toggle('active');  // Toggling on hamburger icon
-    document.querySelector('nav').classList.toggle('active');  // Toggling on nav menu
+    document.querySelector('nav ul').classList.toggle('active');  // Toggling on nav menu
 }
