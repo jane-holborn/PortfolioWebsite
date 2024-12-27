72119fd59136c57d98ed1f341268e90bb93ac6ee
Jane Holborn <janeholborn@hotmail.com>
2024-12-27 10:33:13 +0800

    Resolved minor accessibility issues.

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function toggleMenu() {
     document.querySelector('.hamburgerIcon').classList.toggle('active');  // Toggling on hamburger icon
     document.querySelector('nav ul').classList.toggle('active');  // Toggling on nav menu
 }
+
+document.querySelector('.hamburgerIcon').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' || event.key === ' ') {
+        toggleMenu();
+    }
+});
